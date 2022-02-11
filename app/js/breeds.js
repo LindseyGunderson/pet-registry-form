68f305d6e1bcb65fa0143c1cae8675c0d96f5ef7
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2022-02-11 12:59:35 -0800

    Fixed issue with toggling buttons.
    Issue when Cat/Dog or Female/Male buttons were toggled, both would be selected and no toggle would take affect. Resolved issue by renaming name  variables for each section. Now buttons toggle as expected. This also correct the issue with filtering the petType data dynamically when different pet type is selected.

---
+++ app/js/breeds.js
@@ -0,0 +1,21 @@
+let dogBreeds = [
+
+    "Australian Shepherd",
+    "American Cocker Spaniel",
+    "Akita",
+    "Airedale Terrier",
+    "Border Collie",
+    "Can't find breed?"
+
+];
+
+let catBreeds = [
+    "Ragdoll",
+    "Exotic Shorthair",
+    "British Shorthair",
+    "Persian",
+    "Maine Coon",
+    "American Shorthair",
+    "Can't find breed?"
+
+];
--- app/js/script.js
+++ app/js/script.js
@@ -10,6 +10,14 @@ const radioBtn = document.querySelector(".breed-input"),
   mixBreedTextbox = document.querySelector("#mixedBreedTextbox"),
   noBreedInput = document.querySelector("#noBreedRadioBtn");
 
+//  get all required elements for searching for a breed
+const searchWrapper = document.querySelector(".search-wrapper"),
+searchInput = searchWrapper.querySelector("#searchInput"),
+searchResults = searchWrapper.querySelector("#breedOptions");
+
+
+
+
 
 /* ----- Breed selection with radio buttons -----
 
@@ -35,17 +43,69 @@ mixedBreedInput.addEventListener("click", () => {
 
 /* ----- Determine which type of pet was chosen -----
 
-    1. Add an event listen for selected pet type
+    1. Determine which pet type was chosen
     2. if the radio button selected is "cat", value will be passed to the breeds filter
-    3. breeds filter will only show the list for the selected type of pet ex. only cat breed types will be shown
+    3. breeds filter will only show the list for the selected type of pet ex. only cat breed types will be shown if "cat" is selected
 
 */
 
 /* ----- Add breeds dynamically to datalist for search -----
 
     1. Add event listener to track users input into input text field
-    2. 
-    2. if the radio button selected is "cat", value will be passed to the breeds filter
+    2. Note*: if the radio button selected is "cat", value will be passed to the breeds filter
     3. breeds filter will only show the list for the selected type of pet ex. only cat breed types will be shown
-
+    4. User's input will auto complete closest match from filtered list
+    5. Option for Can't find breed added to list.
 */
+
+searchInput.addEventListener("keyup", () => {
+    // emtpy array to store results
+    let userResults = [];
+  
+    // gather the users input into the search box
+    let userInput = searchInput.value;
+  
+    // if there are results
+    if (userInput.length) {
+
+        // check to see which pet type was selected
+        if (dogType.checked) {
+            // filter the results that match the user's input
+            userResults = dogBreeds.filter((breeds) => {
+            return breeds.toLowerCase().includes(userInput.toLowerCase());
+            });
+        } else {
+            userResults = catBreeds.filter((breeds) => {
+            return breeds.toLowerCase().includes(userInput.toLowerCase());
+            });
+      }
+    }
+  
+    console.log(userResults);
+
+    // Show the results
+    showResults(userResults);
+  });
+  
+  function showResults(results) {
+    if (!results.length) {
+      // searchResults.classList.remove('show');
+      console.log(results);
+      console.log("no results");
+      return (searchResults.innerHTML = `<option value="Can't Find it?">`);
+    }
+  
+    // take the breed list results
+    const breedList = results
+
+        // map through the results
+      .map((breed) => {
+
+        // return the breeds.js file and add them to the datalist as options
+        return `<option value="${breed}">`;
+      })
+      .join("");
+
+    // add the list and show the results
+    searchResults.innerHTML = `${breedList}`;
+  }
